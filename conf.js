33574fa6082934a08aef0d782b92e9101fe69000
symant233 <symant233@gmail.com>
2021-02-22 11:32:25 +0800

    css美化, 更改默认搜索引擎... - 删除cfworker节点 - 删除quora

--- conf.js
+++ conf.js
@@ -28,9 +28,7 @@ jsproxy_config({
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        ['s1-' + location.host]: 1,
         ['s2-' + location.host]: 1,
-        ['s3-' + location.host]: 1,
       }
     }
   },
